dec628133e6c15ec7f69f8b3a21a8e8b4a1263ca
v_utsav <utsav.v@ahduni.edu.in>
2026-04-24 18:36:36 +0530

    changed the location of db.json to deploy the website on gh pages

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:3002/users';
+const API_URL = 'https://my-json-server.typicode.com/v-utsav/crud-app';
 
 export const addUser = async (data) => {
     try{
