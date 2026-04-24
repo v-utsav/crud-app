ad07546de05a0e83be8e3c259b1af0fef1ba839f
v_utsav <utsav.v@ahduni.edu.in>
2026-04-24 20:06:48 +0530

    changed URL in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:3002/users")
+    fetch(process.env.REACT_APP_API_URL)
       .then((res) => res.json())
       .then((data) => {
         setUsers(data);
